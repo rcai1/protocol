9e5454cb6ab9ccecc77d9516b06d42dc047b59a2
Prasad Tare <prasad.tare@gmail.com>
2019-08-27 10:42:24 -0400

    Prettier
    Signed-off-by: Prasad Tare <prasad.tare@gmail.com>

--- sponsor-dapp-v2/src/views/Borrow.js
+++ sponsor-dapp-v2/src/views/Borrow.js
@@ -21,11 +21,11 @@ function useMaxTokensThatCanBeCreated(tokenAddress, marginAmount) {
   const tokenValue = useCacheCall(tokenAddress, "calcTokenValue");
 
   const dataFetched = derivativeStorage && newExcessMargin && updatedPrice && tokenValue;
-    if (marginAmount === "") {
-        return { ready: true, maxTokens: toBN("0")};
-    }
+  if (marginAmount === "") {
+    return { ready: true, maxTokens: toBN("0") };
+  }
   if (!dataFetched) {
-        return { ready: false, maxTokens: toBN("0")};
+    return { ready: false, maxTokens: toBN("0") };
   }
 
   const fpScalingFactor = toBN(toWei("1"));
@@ -68,7 +68,7 @@ function useMaxTokensThatCanBeCreated(tokenAddress, marginAmount) {
 
   // The contract doesn't allow the sponsor to escape default and create tokens at once: they have to first
   // deposit and then create. I.e., a negative `marginLimit` means that remargining would default the contract.
-  return { ready: true, maxTokens: marginLimit.isNeg() ? toBN("0") : limit};
+  return { ready: true, maxTokens: marginLimit.isNeg() ? toBN("0") : limit };
 }
 
 function Borrow(props) {
@@ -89,13 +89,12 @@ function Borrow(props) {
 
   const data = useCollateralizationInformation(tokenAddress, "");
   data.updatedUnderlyingPrice = useCacheCall(tokenAddress, "getUpdatedUnderlyingPrice");
-  const {ready, maxTokens} = useMaxTokensThatCanBeCreated(tokenAddress, marginAmount);
+  const { ready, maxTokens } = useMaxTokensThatCanBeCreated(tokenAddress, marginAmount);
   if (!data.ready || !data.updatedUnderlyingPrice || !ready) {
     return <div>Loading borrow data</div>;
   }
 
-  const allowedToProceed =
-    marginAmount !== "" && tokenAmount !== "" && toBN(toWei(tokenAmount)).lte(maxTokens);
+  const allowedToProceed = marginAmount !== "" && tokenAmount !== "" && toBN(toWei(tokenAmount)).lte(maxTokens);
 
   const format = createFormatFunction(drizzle.web3, 4);
 
